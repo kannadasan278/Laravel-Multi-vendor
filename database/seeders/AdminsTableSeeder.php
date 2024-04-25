1b2547de325818cbd720d054b23065ba950d5a3b
Dhasan KD <kannadhasancse278@gmail.com>
2024-04-25 10:40:03 +0530

    Update AdminsTableSeeder.php

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -23,7 +23,7 @@ class AdminsTableSeeder extends Seeder
         $adminRecords = [
             [
                 'id'        => 1,
-                'name'      => 'Ahmed Yahya',
+                'name'      => 'kannadasan',
                 'type'      => 'superadmin',
                 'vendor_id' => 0, // `vendor_id` is zero 0 because 'type' is not 'vendor' (it's actually 'superadmin')
                 'mobile'    => '9800000000',
@@ -36,11 +36,11 @@ class AdminsTableSeeder extends Seeder
             
             [
                 'id'        => 2,
-                'name'      => 'John Singh - Vendor',
+                'name'      => 'vel - Vendor',
                 'type'      => 'vendor',
                 'vendor_id' => 1, // `vendor_id` is one 1 because 'type' is 'vendor'
                 'mobile'    => '9700000000',
-                'email'     => 'john@admin.com',
+                'email'     => 'vel@admin.com',
                 'password'  => '$2a$12$xvkjSScUPRexfcJTAy9ATutIeGUuRgJrjDIdL/.xlrddEvRZINpeC', // This is the encryption of '123456'    // using https://bcrypt-generator.com/
                 'image'     => '',
                 'status'    => 1, // Our authentication logic in the login() method in the AdminController won't allow this admin logging in in case of 'status' = 0
